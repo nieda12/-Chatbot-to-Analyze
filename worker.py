69795fbaac3dfa71d93a02770b04668d3407ce7b
tahta nieda <124947068+nieda12@users.noreply.github.com>
2024-03-07 08:18:06 +0000

    initial commit

--- worker.py
+++ worker.py
@@ -22,9 +22,9 @@ llm_embeddings = None
 def init_llm():
     global llm, llm_embeddings
     # Initialize the language model with the OpenAI API key
-    api_key="YOUR API KEY"
+    api_key=""
     # ---> TODO: write your code here <----
-    
+    llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=api_key)
     # Initialize the embeddings for the language model
     llm_embeddings = OpenAIEmbeddings(openai_api_key = api_key)
 
@@ -32,6 +32,8 @@ def init_llm():
 def process_document(document_path):
     global conversation_retrieval_chain, llm, llm_embeddings
     # Load the document
+    loader = PyPDFLoader(document_path)
+    
     # ---> TODO: write your code here <---
     
     documents = loader.load()
@@ -52,7 +54,7 @@ def process_prompt(prompt):
     # Pass the prompt and the chat history to the conversation_retrieval_chain object
     result = conversation_retrieval_chain({"question": prompt, "chat_history": chat_history})
     # ---> TODO: Append the prompt and the bot's response to the chat history <--
-
+    chat_history.append((prompt, result["answer"]))
     # Return the model's response
     return result['answer']
 
